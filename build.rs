9b39634447e22ab753fbfef4d2296407e5b141e8
ringsaturn <ringsaturn.me@gmail.com>
2022-11-20 23:53:02 +0800

    bypass build pb

--- build.rs
+++ build.rs
@@ -1,10 +1,15 @@
+use std::env;
 use std::process::Command;
 
 fn main() {
-    prost_build::Config::new()
-        .out_dir("src/gen/")
-        .compile_protos(&["./tzinfo.proto"], &["."])
-        .unwrap();
+    match env::var_os("TZF_RS_BUILD_PB") {
+        Some(_) => prost_build::Config::new()
+            .out_dir("src/gen/")
+            .compile_protos(&["./tzinfo.proto"], &["."])
+            .unwrap(),
+        None => println!("no need for pb"),
+    }
+
     Command::new("cargo")
         .args(&["fmt", "--", "src/*.rs"])
         .status()
