27a70225d19b0677798c03d971bebdc005f04f65
ringsaturn <ringsaturn.me@gmail.com>
2022-11-29 23:32:22 +0800

    add cli tool (#12)

---
+++ src/bin/tzf.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+use tzf_rs::DefaultFinder;
+
+#[derive(Parser, Debug)]
+#[command(name = "tzf")]
+struct Cli {
+    /// longitude
+    #[arg(long)]
+    lng: f64,
+
+    /// latitude
+    #[arg(long)]
+    lat: f64,
+}
+
+pub fn main() {
+    let cli = Cli::parse();
+    let finder = DefaultFinder::new();
+    println!("{:?}", finder.get_tz_name(cli.lng, cli.lat));
+}
