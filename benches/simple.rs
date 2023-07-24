7a3c1c704ff1589e47042a94e96fc765eb59c78c
ringsaturn <ringsaturn.me@gmail.com>
2023-07-24 12:31:27 +0800

    Append simple benchmark for CI (#96)

---
+++ benches/simple.rs
@@ -0,0 +1,18 @@
+#![feature(test)]
+#[cfg(test)]
+mod benches_default {
+
+    use cities_json;
+    use tzf_rs::DefaultFinder;
+    extern crate test;
+    use test::Bencher;
+    #[bench]
+    fn bench_default_finder_random_city(b: &mut Bencher) {
+        let finder: DefaultFinder = DefaultFinder::new();
+
+        b.iter(|| {
+            let city = cities_json::get_random_cities();
+            let _ = finder.get_tz_name(city.lng, city.lat);
+        });
+    }
+}
